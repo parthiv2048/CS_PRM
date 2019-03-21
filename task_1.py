e78a250add6d31ca5667271b0c711fd0825fa1f4
Parthiv Ganguly <parthivganguly@Parthivs-MacBook-Air.local>
2019-03-21 10:47:16 +0530

    Complete TASK 1.6

--- task_1.py
+++ task_1.py
@@ -37,3 +37,23 @@ for student_details in students:
     student_name = student_details[:student_details.index("#")]
     if query_name in student_name:
         print(student_details[:student_details.index("#")] + " " * (22-len(student_details[:student_details.index("#")])) + student_details[student_details.index("#")+1:])
+
+# TASK 1.6
+students = []
+while True:
+    students_left = input("Are there any more students left? (Y/N): ")
+    if students_left == "N":
+        break
+    name = input("What is your name?: ")
+    email = input("What is your email address?: ")
+    date = input("What is your date of birth? (DD/MM/YY): ")
+    id = input("What is your student ID?: ")
+    student_details = [name, email, date, id]
+    students.append(student_details)
+
+print("Student Name          Email Address          Date of birth          Student ID")
+for student_details in students:
+    print(student_details[0] + " " * (22-len(student_details[0])), end="")
+    print(student_details[1] + " " * (23-len(student_details[0])), end="")
+    print(student_details[2] + " " * (23-len(student_details[0])), end="")
+    print(student_details[3])
